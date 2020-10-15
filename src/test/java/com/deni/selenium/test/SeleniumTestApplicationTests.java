89bc9ef8d76ef490f9ca15e9a05c13fc1092552c
Deni Gunawan <artdengun@gmail.com>
2020-10-15 09:41:20 +0700

    selenium-test

---
+++ src/test/java/com/deni/selenium/test/SeleniumTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deni.selenium.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
